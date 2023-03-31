83243425b9bdd193387470ae2632f2525f5254d3
safeaf <transiencymail@gmail.com>
2023-03-31 08:13:15 -0600

    made schema.rb work for sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,11 @@
 require 'active_record'
 
-
 ActiveRecord::Base.establish_connection(
-  adapter: 'mysql2',
-  pool: 5,
-  host: '192.168.1.10',
-  username: 'weather',
-  password: 'd41d8cd98f00b204e9800998ecf8427e',
-  port: 3306,
-  database: 'weather',
+  adapter: 'sqlite3',
+  database: 'idaweather_prod.db'
 )
+
+
 
 ActiveRecord::Schema.define do
 
